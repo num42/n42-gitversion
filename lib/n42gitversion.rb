089e86358d6f75d8459d71a5fa7eaa42c9582451
Wolfgang Lutz <wolfgang@lutz-wiesent.de>
2015-08-04 15:05:30 +0200

    move to thor

--- lib/n42gitversion.rb
+++ lib/n42gitversion.rb
@@ -1,7 +1,19 @@
 require "n42gitversion/version"
+require "n42gitversion/cli"
 
 module N42gitversion
-  def self.version
-    puts `git --git-dir=".git" --work-tree="." describe --tags --dirty | sed -e 's/^v//' -e 's/g//'`
+  class Versioner 
+    def version
+      puts `git --git-dir=".git" --work-tree="." describe --tags --dirty | sed -e 's/^v//' -e 's/g//'`
+    end
+    
+    def shortVersion
+      puts `git --git-dir=".git" --work-tree="." describe --tags --abbrev=0`
+    end
+
+    def fullVersion
+      puts `git --git-dir=".git" --work-tree="." rev-list master | wc -l`
+    end
+
   end
 end
--- lib/n42gitversion/version.rb
+++ lib/n42gitversion/version.rb
@@ -1,3 +1,3 @@
 module N42gitversion
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
