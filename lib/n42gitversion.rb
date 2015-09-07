591270f3f1b38c5650a1d09476e2038db027c5bf
Daniel Dengler <me@ddengler.com>
2015-09-07 10:20:20 +0200

    trim whitespace

--- lib/n42gitversion.rb
+++ lib/n42gitversion.rb
@@ -4,15 +4,15 @@ require "n42gitversion/cli"
 module N42gitversion
   class Versioner
     def version
-      `git --git-dir=".git" --work-tree="." describe --tags --dirty | sed -e 's/^v//' -e 's/g//'`
+      strip(`git --git-dir=".git" --work-tree="." describe --tags --dirty | sed -e 's/^v//' -e 's/g//'`)
     end
 
     def shortVersion
-      `git --git-dir=".git" --work-tree="." describe --tags --abbrev=0 | sed -e 's/^v//'`
+      strip(`git --git-dir=".git" --work-tree="." describe --tags --abbrev=0 | sed -e 's/^v//'`)
     end
 
     def fullVersion
-      number_of_commits = `git --git-dir=".git" --work-tree="." rev-list master | wc -l`
+      number_of_commits = strip(`git --git-dir=".git" --work-tree="." rev-list master | wc -l`)
       "#{majorVersion}#{number_of_commits}"
     end
 
