70c8890a05b06597b1d07a57417531ef58651178
Daniel Dengler <me@ddengler.com>
2015-09-07 10:18:17 +0200

    move output to cli

--- lib/n42gitversion.rb
+++ lib/n42gitversion.rb
@@ -4,16 +4,16 @@ require "n42gitversion/cli"
 module N42gitversion
   class Versioner
     def version
-      puts `git --git-dir=".git" --work-tree="." describe --tags --dirty | sed -e 's/^v//' -e 's/g//'`
+      `git --git-dir=".git" --work-tree="." describe --tags --dirty | sed -e 's/^v//' -e 's/g//'`
     end
 
     def shortVersion
-      puts `git --git-dir=".git" --work-tree="." describe --tags --abbrev=0 | sed -e 's/^v//'`
+      `git --git-dir=".git" --work-tree="." describe --tags --abbrev=0 | sed -e 's/^v//'`
     end
 
     def fullVersion
       number_of_commits = `git --git-dir=".git" --work-tree="." rev-list master | wc -l`
-      puts "#{majorVersion}#{number_of_commits}"
+      "#{majorVersion}#{number_of_commits}"
     end
 
     def majorVersion
--- lib/n42gitversion/cli.rb
+++ lib/n42gitversion/cli.rb
@@ -2,24 +2,20 @@ require 'thor'
 
 module N42gitversion
   class VersionCLI < Thor
-    
-    
-    
-
     N42gitversion
     desc "version", "prints the version"
     def version
-        N42gitversion::Versioner.new.version
+        puts N42gitversion::Versioner.new.version
     end
 
     desc "shortVersion", "prints the short version"
     def shortVersion
-        N42gitversion::Versioner.new.shortVersion
+        puts N42gitversion::Versioner.new.shortVersion
     end
-    
+
     desc "fullVersion", "prints the  full version"
     def fullVersion
-        N42gitversion::Versioner.new.fullVersion
+        puts N42gitversion::Versioner.new.fullVersion
     end
   end
 end
